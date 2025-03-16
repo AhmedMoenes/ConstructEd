82aba7c8ba09c2cf3fe4cad0244a6b0f27480de5
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-03-16 02:22:25 +0200

    Add payment handling to PaymentCourse and update Create view
    Updated PaymentCourse.cs to include PaymentId and CourseId properties with data annotations.
    Transformed Create.cshtml from a course creation form to a payment form, changing the model to PaymentViewModel.
    Introduced a modal for payment processing with fields for card details and amount, and added JavaScript for input formatting.

--- ConstructEd.Model/PaymentCourse.cs
+++ ConstructEd.Model/PaymentCourse.cs
@@ -1,10 +1,15 @@
-﻿namespace ConstructEd.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConstructEd.Models
 {
     public class PaymentCourse
     {
+        [Key, Column(Order = 0)]
         public int PaymentId { get; set; }
         public Payment Payment { get; set; }
 
+        [Key, Column(Order = 1)] 
         public int CourseId { get; set; }
         public Course Course { get; set; }
     }
